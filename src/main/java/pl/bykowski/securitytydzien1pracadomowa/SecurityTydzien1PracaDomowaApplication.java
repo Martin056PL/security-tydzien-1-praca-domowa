bf22758a483d79a2196f11b41be71947791b1888
Kamil Wawer <kamil.wawer@pollub.edu.pl>
2020-01-30 17:01:50 +0100

    Initial commit

---
+++ src/main/java/pl/bykowski/securitytydzien1pracadomowa/SecurityTydzien1PracaDomowaApplication.java
@@ -0,0 +1,13 @@
+package pl.bykowski.securitytydzien1pracadomowa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityTydzien1PracaDomowaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityTydzien1PracaDomowaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/bykowski/securitytydzien1pracadomowa/SecurityTydzien1PracaDomowaApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.bykowski.securitytydzien1pracadomowa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityTydzien1PracaDomowaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
